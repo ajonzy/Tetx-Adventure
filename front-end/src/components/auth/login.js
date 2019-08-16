be5009ceea745bf796ef81c9bf8166d947ea4d99
gleekzorp <gleek.dan@gmail.com>
2019-08-16 16:27:21 -0600

    Added login functionality

--- front-end/src/components/auth/login.js
+++ front-end/src/components/auth/login.js
@@ -1,22 +1,68 @@
 import React, { Component } from 'react';
 
+import Home from "../pages/home"
+
 export default class Login extends Component {
   constructor(props) {
     super(props)
     
     this.state = {
-      userName: '',
-      password: ''
+      username: '',
+      password: '',
+      user: undefined
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChange(event) {
+    console.log(event.target.value)
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    fetch("http://127.0.0.1:5000/users/verification", {
+      method: "POST",
+      headers: {
+          "Content-type": "application/json"
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data == "User Verified") {
+        fetch("http://127.0.0.1:5000/users/get/test1", {
+          method: "GET",
+          headers: {
+            "Content-type": "application/json"
+          }
+        })
+        .then(response => response.json())
+        .then(data => {
+          this.setState({
+            user: data
+          })
+        })
+      }
+    })
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
         <div className="login-register-container">
           <div className="login-register-wrapper">
-            <input className="userName" type="text"/>
-            <input className="password" type="password"/>
-            <button className="login">LOGIN</button>
+            <input className="username" type="text" name="username" onChange={this.handleChange}/>
+            <input className="password" type="password" name="password" onChange={this.handleChange}/>
+            <button className="register" onClick={this.handleSubmit}>LOGIN</button>
+            {user ? <Home user={this.state.user} /> : null}
           </div>
         </div>
       </div>
